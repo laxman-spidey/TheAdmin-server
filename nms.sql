35f066390e65eb12277ced495674f1236d86eba2
Laxman Spidey <mittu.thefire@gmail.com>
2016-08-06 09:54:41 +0000

    added shift id link to attendance

--- nms.sql
+++ nms.sql
@@ -3,13 +3,15 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2016 at 03:54 PM
+-- Generation Time: Aug 06, 2016 at 08:47 AM
 -- Server version: 5.5.49-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE DATABASE `nms`;
+USE `nms`;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,21 +32,12 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   `attendance_id` int(11) NOT NULL AUTO_INCREMENT,
   `staff_id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `time_in` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `time_out` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `shift_id` int(11) NOT NULL,
+  `time_in` timestamp,
+  `time_out` timestamp,
   PRIMARY KEY (`attendance_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
---
--- Dumping data for table `attendance`
---
-
-INSERT INTO `attendance` (`attendance_id`, `staff_id`, `date`, `time_in`, `time_out`) VALUES
-(1, 1, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 4, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(3, 4, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(4, 5, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(5, 6, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -105,11 +98,21 @@ CREATE TABLE IF NOT EXISTS `roaster` (
 
 CREATE TABLE IF NOT EXISTS `shift` (
   `shift_id` int(11) NOT NULL,
-  `shift` varchar(5) NOT NULL,
-  `time_in` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `time_out` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `shift` varchar(10) NOT NULL,
+  `description` varchar(20) NOT NULL,
+  `time_in` time NOT NULL,
+  `time_out` time NOT NULL,
   PRIMARY KEY (`shift_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shift`
+--
+
+INSERT INTO `shift` (`shift_id`, `shift`, `description`, `time_in`, `time_out`) VALUES
+(1, 'Shift-A', 'morning shift', '08:00:00', '14:00:00'),
+(2, 'Shift-B', 'Afternoon shift', '14:00:00', '20:00:00'),
+(3, 'Shift-C', 'Night shift', '20:00:00', '08:00:00');
 
 -- --------------------------------------------------------
 
@@ -160,7 +163,10 @@ CREATE TABLE IF NOT EXISTS `swap_rule` (
 -- Constraints for dumped tables
 --
 
+-- Constraints for table `leave`
 --
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`shift_id`) REFERENCES `shift` (`shift_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 -- Constraints for table `leave`
 --
 ALTER TABLE `leave`
@@ -192,3 +198,16 @@ ALTER TABLE `swap_request_sent`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`attendance_id`, `staff_id`, `shift_id`, `date`, `time_in`, `time_out`) VALUES
+(1, 1, 1, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 4, 1, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 4, 1, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 5, 1, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(5, 6, 1, '0000-00-00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
